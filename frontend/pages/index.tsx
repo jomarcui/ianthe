0d19c826508e351ab1a06d0a2d7d62200097cbcf
jomarcui <cuijomar@gmail.com>
2022-07-22 02:27:26 +0800

    🍱 Added public images and icons

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,14 @@
+import { Box } from '@mui/material';
+import Image from 'next/image';
 import Dashboard from '../components/Dashboard';
 import Layout from '../components/Layout';
 
 const Home = () => (
   <Layout>
-    <Dashboard />
+    {/* <Dashboard /> */}
+    <Box sx={{ my: 2 }}>
+      <Image src="/ianthe.png" alt="ianthe" width="100" height="100" />
+    </Box>
   </Layout>
 );
 
