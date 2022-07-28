271da54abbe439d1278c2adda7dd6c24a5574264
jomarcui <cuijomar@gmail.com>
2022-07-29 00:29:27 +0800

    💩 Rewrite logic pertaining to loading of data

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,12 +33,14 @@ const Home = () => {
 
   if (isLoading) {
     return (
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={isLoading}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
+      <Layout>
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={isLoading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </Layout>
     );
   }
 
