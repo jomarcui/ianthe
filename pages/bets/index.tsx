e65a8980d564431bd5f12ab7418462f7a14f66c8
jomarcui <cuijomar@gmail.com>
2022-08-25 00:14:48 +0800

    🚧 Added temporary landing pages for bets and transactions

---
+++ pages/bets/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import ComponentsLayout from '../../components/Layout';
+
+const Bets: NextPage = () => {
+  return <ComponentsLayout>Coming soon.</ComponentsLayout>;
+};
+
+export default Bets;
---
+++ pages/transactions/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import ComponentsLayout from '../../components/Layout';
+
+const Transactions: NextPage = () => {
+  return <ComponentsLayout>Coming soon.</ComponentsLayout>;
+};
+
+export default Transactions;
